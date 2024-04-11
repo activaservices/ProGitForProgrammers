521a8f54407b26803121d5b0707b627a6876999a
activaservices <activa-services@group-activa.com>
2024-04-11 15:44:46 +0000

    Add Multiply method

--- Calculator.cs
+++ Calculator.cs
@@ -7,4 +7,13 @@ namespace ProGitForProgrammers
         {
             return left + right;
         }
+    //add the substract method to the calculator class
+    public int Subtract (int left, int right)
+        {
+            return left - right;
+        }
+    public int Multiply(int left, int right)
+ {
+     return left * right;
+ }
 }
