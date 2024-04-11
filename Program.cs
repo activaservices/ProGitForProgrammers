adde533a8bb0ed2a06842839d5d8b0525abcc8f1
activaservices <activa-services@group-activa.com>
2024-04-11 13:21:50 +0000

    Add writeline indicating we are in command line

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ public class Program
     {
         Console.WriteLine("Hello World!");
         Console.WriteLine("I just added this in Visual Studio");
+		Console.WriteLine("I just added this in the command line repo");
     }
 }
