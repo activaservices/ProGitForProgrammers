3ad4b785cbca2e31334f133731bdb5ebfb151e10
activaservices <activa-services@group-activa.com>
2024-04-12 08:22:31 +0000

    Added Reference ID to the Book's class

--- Book.cs
+++ Book.cs
@@ -2,5 +2,6 @@ public class Book
 {
     public string Title{get;set;}
 	public List <string> Authors{get;set;}
+	public List <string> ReferenceID{get;set;}
 	public DateTime PuublicationDate {get;set;}
 } 
